83a0d23c7ff8657f7c3fa959e0dd712727f6470c
Ovsjah <ovsjah@gmail.com>
2019-02-24 16:22:45 +0200

    Refactor User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   def authenticated?(type, token)
     digest = send("#{type}_digest")
     return false unless digest
-    User.digest(token) == digest
+    self.class.digest(token) == digest
   end
 
 private
@@ -33,8 +33,8 @@ private
     end
 
     def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
+      self.remember_token = self.class.new_token
+      update_attribute(:remember_digest, self.class.digest(remember_token))
     end
 
     def forget
